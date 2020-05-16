aa0dcc127cd7be1bc3f77a4c787bd6d52078f61b
Yuya Kaido <yuyakaido@gmail.com>
2020-05-16 20:49:44 +0900

    Add StorybookActivity

--- sample/src/main/java/com/yuyakaido/android/storybook/sample/MainActivity.kt
+++ sample/src/main/java/com/yuyakaido/android/storybook/sample/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.yuyakaido.android.storybook.sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yuyakaido.android.storybook.StorybookActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, StorybookActivity::class.java))
+        finish()
     }
 
 }
---
+++ storybook/src/main/java/com/yuyakaido/android/storybook/StorybookActivity.kt
@@ -0,0 +1,13 @@
+package com.yuyakaido.android.storybook
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StorybookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storybook)
+    }
+
+}
